be4b9e6ac1edcb3a8053884d4fea5d0b36716316
Joe Mathes <jmathes@gmail.com>
2014-01-21 10:57:25 -0800

    some info on car states I gathered and better logging for sleep timeout finder

--- find_sleep_timeout.py
+++ find_sleep_timeout.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 import tesla
 import time
+import logging
 from os import environ
+
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG)
 
 email = environ['TESLA_EMAIL']
 password = environ['TESLA_PASSWORD']
@@ -16,14 +19,14 @@ def is_online():
 ub = 1
 while is_online():
     ub *= 2
-    print ub
+    logging.info(ub)
     time.sleep(ub)
 
 lb = ub / 2
 
 
 while True:
-    print "timeout between %s and %s" % (lb, ub)
+    logging.info("timeout between %s and %s", lb, ub)
     test = (lb + ub) / 2
     car.wake_up()
     time.sleep(test)
